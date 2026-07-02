7b9be5d74205dba8ecae7deb82bcdeb7e32a2201
mustela <ejankowski@gmail.com>
2026-07-02 15:20:28 +0200

    adding telemetry

--- app/main.py
+++ app/main.py
@@ -7,16 +7,16 @@ from rollbar.logger import RollbarHandler
 from rollbar.contrib.fastapi import ReporterMiddleware as RollbarMiddleware
 from fastapi_pagination import add_pagination
 from tessera_sdk.server.health import get_livez_readyz_router
-
+from prometheus_fastapi_instrumentator import Instrumentator
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from app.telemetry import setup_tracing
 from app.exceptions.handlers import register_exception_handlers
 from app.infra.logging_config import get_logger
 from app.db import db_manager
-from app.utils.metrics import PrometheusMiddleware, metrics
 from tessera_sdk.server.dependencies.auth import get_current_user
 from fastapi.openapi.utils import get_openapi
 from app.models.user import User
+from prometheus_fastapi_instrumentator import Instrumentator
 
 SKIP_AUTH_PATHS = ["/livez", "/readyz", "/metrics"]
 
@@ -71,10 +71,6 @@ def create_app(testing: bool = False, auth_middleware=None) -> FastAPI:
             skip_paths=SKIP_AUTH_PATHS,
             user_service_factory=user_service_factory,
         )
-
-        # Setting metrics middleware
-        app.add_middleware(PrometheusMiddleware, app_name=settings.app_name)
-        app.add_route("/metrics", metrics)
 
     else:
         logger.info("Main: No authentication middleware")
@@ -131,6 +127,9 @@ settings = get_settings()
 if settings.otel_enabled:
     tracer_provider = setup_tracing()  # Or use env/config
     FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer_provider)
+    Instrumentator(
+        excluded_handlers=["^/$", "/livez", "/readyz", "/metrics", "none"],
+    ).instrument(app).expose(app)
 
 
 @app.get("/")
